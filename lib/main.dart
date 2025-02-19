642af3220ca88db3e6d59de2339e9035b0ffdc97
KARLA_ALEJANDRA_BALDERRAMA_PADILLA <a.22308051281149@cbtis128.edu.mx>
2025-02-19 20:33:24 +0000

    enviar practica 6

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,8 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // Application name
-      title: 'Flutter Hello World',
+      title: 'Balderrama Flutter',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
@@ -17,25 +18,43 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Balderrama AppBar '),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              // Lo que esta entre llaves son las acciones
+            },
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.more_vert,
+            ),
+            onPressed: () {},
+          )
+        ],
+        backgroundColor: Colors.lightBlue,
+
         // The title text which will be shown on the action bar
         title: Text(title),
+        centerTitle: true,
+        leading: const Icon(Icons.account_box),
       ),
-      body: Center(
+      body: const Center(
         child: Text(
-          'Hello, World!',
+          'Hola, Karla Alejandra Balderrama Padilla',
         ),
       ),
     );
